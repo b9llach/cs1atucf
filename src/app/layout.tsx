bb5d6a5c58ac9cd33a9f09d7f86584209169ec5a
b9llach <billy.bellach@gmail.com>
2025-01-09 15:14:42 -0500

    analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
+import { Analytics } from "@vercel/analytics/react"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -36,6 +37,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           {children}
+          <Analytics />
           <div className="fixed bottom-0 left-0 right-0 border-t border-zinc-800 py-2 px-4 bg-black/50 backdrop-blur-sm">
             <p className="text-center text-zinc-400 text-sm">
               made by <a href="https://github.com/b9llach" className="hover:text-white transition-colors">billy</a> | @b9llach
